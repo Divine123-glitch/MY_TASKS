b4d0666c8f6279677323e384b08ed365dae50650
Divine Joshua <osheanun123@gmail.com>
2025-09-29 10:45:22 +0100

    new

--- Tasks/task_1/task1.py
+++ Tasks/task_1/task1.py
@@ -1,57 +1,58 @@
-# # # # #Write a program that prints.
-# # # # #Step1: Input full name
-# # # # #Step2: Input Uni
-# # # # #Step3: Input Local government area
-# # # # #Step4: Input favourite Nigerian food
-# # # # Full_name = input("Enter your full name: ")
-# # # # Uni = input("Enter your university: ")
-# # # # LGA = input("Enter your local government area: ")
-# # # # favourite_food = input("Enter your favourite Nigerian food: ")
-# # # # print(f"\nHere are the details you provided:\n{Full_name}\n{Uni}\n{LGA}\n{favourite_food}")
-# # # # #or
-# # # # Full_name = input("Enter your full name: ")
-# # # # Uni = input("Enter your university: ")
-# # # # LGA = input("Enter your local government area: ")
-# # # # favourite_food = input("Enter your favourite Nigerian food: ")
-# # # # print(f"\nHere are the details you provided:")
-# # # # print(f"Full Name: {Full_name}")
-# # # # print(f"University: {Uni}")
-# # # # print(f"Local Government Area: {LGA}")
-# # # # print(f"Favourite Nigerian Food: {favourite_food}")
+# Write a program that prints.
+# Step1: Input full name
+# Step2: Input Uni
+# Step3: Input Local government area
+# Step4: Input favourite Nigerian food
+Full_name = input("Enter your full name: ")
+Uni = input("Enter your university: ")
+LGA = input("Enter your local government area: ")
+favourite_food = input("Enter your favourite Nigerian food: ")
+print(f"\nHere are the details you provided:\n{Full_name}\n{Uni}\n{LGA}\n{favourite_food}")
+#or
+Full_name = input("Enter your full name: ")
+Uni = input("Enter your university: ")
+LGA = input("Enter your local government area: ")
+favourite_food = input("Enter your favourite Nigerian food: ")
+print(f"\nHere are the details you provided:")
+print(f"Full Name: {Full_name}")
+print(f"University: {Uni}")
+print(f"Local Government Area: {LGA}")
+print(f"Favourite Nigerian Food: {favourite_food}")
 
 
-# # # # #Storing names in variables
-# # # # #Step1: Input name
-# # # # #Step2: Input State of origin
-# # # # name = input("Enter your name: ")
-# # # # state_of_origin = input("Enter your state of origin: ")
-# # # # print(f"\nYour name is {name} and you hail from {state_of_origin}.")
+# Storing names in variables
+# Step1: Input name
+# Step2: Input State of origin
+name = input("Enter your name: ")
+state_of_origin = input("Enter your state of origin: ")
+print(f"\nYour name is {name} and you hail from {state_of_origin}.")
 
-# # # #Program using variable to store
-# # # # Step1: Input full name
-# # # # Step2: Input Class
-# # # # Step3: Input best subject
-# # # full_name = input("Enter your full name: ")
-# # # class_name = input("Enter your class: ")
-# # # best_subject = input("Enter your best subject: ")
-# # # print(f"\nHere are the details you provided:\nFull Name: {full_name}\nClass: {class_name}\nBest Subject: {best_subject}")
-# # # # or
-# # # print(f"\nYour name is {full_name}, you are in {class_name} and your best subject is {best_subject}.")
+# Program using variable to store
+# Step1: Input full name
+# Step2: Input Class
+# Step3: Input best subject
+full_name = input("Enter your full name: ")
+class_name = input("Enter your class: ")
+best_subject = input("Enter your best subject: ")
+print(f"\nHere are the details you provided:\nFull Name: {full_name}\nClass: {class_name}\nBest Subject: {best_subject}")
+# or
+print(f"\nYour name is {full_name}, you are in {class_name} and your best subject is {best_subject}.")
 
 
-# # #Using Triple Quotes 
-# # """
-# # Nigeria thrives with vibrant hue,
-# # Resilient hearts beat strong and true,
-# # From Lagos pulse to Sahel's dew.
-# # Unity in diversity, we stand,
-# # A tapestry of cultures, hand in hand.
-# # From north to south, east to west,
-# # In every challenge, we give our best.
-# # """
+# Using Triple Quotes 
+"""
+Nigeria thrives with vibrant hue,
+Resilient hearts beat strong and true,
+From Lagos pulse to Sahel's dew.
+Unity in diversity, we stand,
+A tapestry of cultures, hand in hand.
+From north to south, east to west,
+In every challenge, we give our best.
+Nigeria, our home, our pride,
+In your embrace, we all reside."""
 
-# #Simple Timetable for a Nigerian Secondary School 
-# #Step1: Input subject names
-# #Step2: Input time slots
+# Simple Timetable for a Nigerian Secondary School 
+# Step1: Input subject names
+# Step2: Input time slots
 print("Welcome to the Nigerian Secondary School Timetable!")
 print("Time\t    8:30-9:30\t    9:30-10:30\t    10:30-11:30\t    11:30-12:30\t    12:30-1:30\t    1:30-2:30\t    2:30-3:30\nMonday\t    Maths\t    English\t    Physics\t    Chemistry\t    Biology\t    Geography\t    History\nTuesday\t    English\t    Maths\t    Chemistry\t    Physics\t    Biology\t    Geography\t    History\nWednesday   Physics\t    Chemistry\t    Maths\t    English\t    Biology\t    History\t    Geography\nThursday    Chemistry\t    Biology\t    English\t    Maths\t    Geography\t    History\t    Physics\nFriday\t    Geography\t    History\t    Maths\t    English\t    Physics\t    Chemistry\t    Biology")
